bb59de76b7c6696f09bc3c549b36e402ecf6ee8e
kuzugr <monmon.syusyo@gmail.com>
2020-02-25 10:26:04 +0900

    PDFを画面表示させる設定を追加

--- app/controllers/basic_pdf_controller.rb
+++ app/controllers/basic_pdf_controller.rb
@@ -1,4 +1,13 @@
 class BasicPdfController < ApplicationController
   def index
+    respond_to do |format|
+      format.pdf do
+        basic_pdf = PracticePdf::BasicPdf.new().render
+        send_data basic_pdf,
+          filename: 'basic_pdf.pdf',
+          type: 'application/pdf',
+          disposition: 'inline' # 画面に表示
+      end
+    end
   end
 end
